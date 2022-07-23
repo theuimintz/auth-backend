cc69057d8352336f4505389e1adf3485420b104a
Uith Mintz <theuimintz@gmail.com>
2022-07-23 12:03:22 +0200

    Minor updates

--- src/main/java/com/auth/controllers/ProfileImageController.java
+++ src/main/java/com/auth/controllers/ProfileImageController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import com.auth.dtos.ProfileImageDTO;
 import com.auth.services.ProfileImageService;
-import com.auth.services.UserService;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
--- src/main/java/com/auth/services/UserService.java
+++ src/main/java/com/auth/services/UserService.java
@@ -3,9 +3,6 @@ package com.auth.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 import com.auth.dtos.UserDTO;
 import com.auth.models.UserModel;
